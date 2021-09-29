479d9f5831303b93fddb720c44b247aaa5631edc
rosalbadif <difonzorosalba@gmail.com>
2021-09-29 08:50:26 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,26 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
+  
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background("gold");
+  angleMode(DEGREES);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  
+  noFill()
+  let myColor = lerpColor(color("tomato"), color("gold"), frameCount/600)
+
+  
+
+  translate(width/2, height/2)
+  rotate(frameCount*2)
+  stroke("gold")
+  line(0,0,500,frameCount*2)
+  stroke(myColor)
+  ellipse(0,0,frameCount,frameCount)
+  
+  if(frameCount === 600) {noLoop()}
+  
+
 }
+
